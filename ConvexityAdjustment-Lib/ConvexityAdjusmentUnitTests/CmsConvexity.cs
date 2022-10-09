0cfead03ff2fb5d433fe34651defddf35b87dd16
Dagalon <63908589+Dagalon@users.noreply.github.com>
2022-10-10 00:50:20 +0200

    # new test for cms convexity adjusment (not end)

--- ConvexityAdjustment-Lib/ConvexityAdjusmentUnitTests/CmsConvexity.cs
+++ ConvexityAdjustment-Lib/ConvexityAdjusmentUnitTests/CmsConvexity.cs
@@ -24,21 +24,21 @@ namespace ConvexityAdjustmentUnitTests
             ql.Quote quoteSpread = new ql.SimpleQuote(basisSpread);
             
             ql.Handle<ql.YieldTermStructure> discountCurve =  new ql.Handle<ql.YieldTermStructure>(new ql.FlatForward(startDate, quoteDiscountRate, dc));
-            ql.Handle<ql.YieldTermStructure> forwardCurve =new ql.Handle<ql.YieldTermStructure>(
-                new ql.ForwardSpreadedTermStructure(discountCurve,  new ql.Handle<ql.Quote>(quoteSpread)));
             
             // Model and process
             ql.OrnsteinUhlenbeckProcess process = new ql.OrnsteinUhlenbeckProcess(k, sigma, 0.0, 0.0);
             ql.HullWhite model = new ql.HullWhite(discountCurve, k, sigma);
             
+            // Dynamics curve
+            ql.Handle<ql.YieldTermStructure> hullWhiteDiscountCurve = new ql.Handle<ql.YieldTermStructure>(new ql.ShortRate1FYieldStructure<ql.HullWhite>(model));
+            
             // Swap product
             var floatingPeriod = new ql.Period(ql.Frequency.Semiannual);
-            var fixedPeriod = new ql.Period(ql.Frequency.Annual);
-            var index = new ql.Euribor6M(forwardCurve);
-            var forwardPeriod = new ql.Period(5, ql.TimeUnit.Years);
+            var payPeriod = new ql.Period(ql.Frequency.Annual);
             var tenorSwap = new ql.Period(5, ql.TimeUnit.Years);
+
+            var discountEngine = new ql.DiscountingSwapEngine(discountCurve, null, startDate, startDate);
             
-       
             // Convexity test
             var periodToCompute = new ql.Period(1, ql.TimeUnit.Years);
             int numberOftimes = 25;
@@ -51,7 +51,7 @@ namespace ConvexityAdjustmentUnitTests
             for (int i = 1; i < numberOftimes; i++)
             {
                 datesToCompute.Add(calendar.advance(datesToCompute[i-1], periodToCompute));
-                deltaTimes.Add(dc.yearFraction(startDate, datesToCompute[i-1]));
+                deltaTimes.Add(dc.yearFraction(startDate, datesToCompute[i]));
                 f0ts.Add(discountCurve.link.forwardRate(deltaTimes[i], deltaTimes[i], ql.Compounding.Continuous, ql.Frequency.NoFrequency).rate());
                 hjmAdjustment.Add(ConvexityAdjustment_Lib.HullWhite.HjmAdjustment(deltaTimes[i-1], deltaTimes[i], k, sigma));
             }
@@ -68,43 +68,65 @@ namespace ConvexityAdjustmentUnitTests
             
             ql.PathGenerator<ql.IRNG> generator = new ql.PathGenerator<ql.IRNG>(process, deltaTimes.Last(), timeSteps, rsg, true, deltaTimes);
             
-            // Pricing Engines
-            var discountEngine = new ql.DiscountingSwapEngine(discountCurve, null, startDate, startDate);
-            
+            // outputs
+            var rateCmsMc = new Dictionary<int, double>();
+            var rateCmsForward = new List<double>();
+
             // Test
             for (int i = 0; i < numberOfSimulations; i++)
             {
                 ql.Sample<ql.IPath> sample = generator.next();
                 var path = (ql.Path)sample.value;
 
-                for (int j = 1; i < numberOftimes; i++)
+                for (int j = 1; j < numberOftimes; j++)
                 {
                     var ri = path[j] + hjmAdjustment[j - 1] + f0ts[j-1];
+                    var endDate = calendar.advance(datesToCompute[j - 1], tenorSwap,
+                        ql.BusinessDayConvention.ModifiedFollowing);
+                    
+                    // Pay date
+                    var tP = calendar.advance(datesToCompute[j - 1], payPeriod,
+                        ql.BusinessDayConvention.ModifiedFollowing);
+                    
+                    // Curves
+                    var dt = dc.yearFraction(startDate, datesToCompute[j - 1]);
+                    ((ql.ShortRate1FYieldStructure<ql.HullWhite>)hullWhiteDiscountCurve.link).updateState(dt, ri);
+                    
+                    // Pricing Engines
+                    var discountEngineHullWhite = new ql.DiscountingSwapEngine(hullWhiteDiscountCurve, null, datesToCompute[j-1], datesToCompute[j-1]);
+                   
+                    ql.Handle<ql.YieldTermStructure> forwardCurve =new ql.Handle<ql.YieldTermStructure>(
+                       new ql.ForwardSpreadedTermStructure(hullWhiteDiscountCurve,  new ql.Handle<ql.Quote>(quoteSpread)));
+                    var index = new ql.Euribor6M(forwardCurve);
+                   
+                    var swap = new ql.MakeVanillaSwap(floatingPeriod, index, 0.0)
+                        .withEffectiveDate(datesToCompute[j-1])
+                        .withFloatingLegTenor(index.tenor())
+                        .withFixedLegTenor(new ql.Period(1, ql.TimeUnit.Years))
+                        .withTerminationDate(endDate)
+                        .withPricingEngine(discountEngineHullWhite).value();
+
+                    double payoffMc = discountCurve.link.discount(dt) * swap.fairRate();
+
+                    if (rateCmsMc.ContainsKey(datesToCompute[j - 1].serialNumber()))
+                    {
+                        rateCmsMc[datesToCompute[j - 1].serialNumber()] += payoffMc / numberOfSimulations;
+                    }
+                    else
+                    {
+                        rateCmsMc.Add(datesToCompute[j - 1].serialNumber(), payoffMc / numberOfSimulations);
+                    }
+                    
+                    if (i == 0)
+                    {
+                        swap.setPricingEngine(discountEngine);
+                        double payoffDiscount = swap.fairRate();
+                        rateCmsForward.Add(payoffDiscount);
+                    }
+
                 }
+                
             }
-            
-            
-            // foreach (var date in datesToCompute)
-            // {
-            //     var endDate = calendar.advance(date, tenorSwap);
-            //     var swap = new ql.MakeVanillaSwap(floatingPeriod, index, 0.0)
-            //         .withEffectiveDate(date)
-            //         .withFloatingLegTenor(index.tenor())
-            //         .withFixedLegTenor(new ql.Period(1, ql.TimeUnit.Years))
-            //         .withTerminationDate(endDate)
-            //         .withPricingEngine(discountEngine).value();
-            //     
-            //     var npv = swap.NPV();
-            //     var rate = swap.fairRate();
-            //
-            //     swap.fixedRate = rate;
-            //     var posNpv = swap.NPV();
-            //
-            //     for (var i = 0; i < numberOfSimulations; i++)
-            //     {
-            //         
-            //     }
-            // }
 
         }
     }
